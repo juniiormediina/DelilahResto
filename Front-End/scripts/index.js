1af4f2a5c0025a59d101fa476e39875628cbc1f8
juniiormediina <juniiormediina@gmail.com>
2020-10-10 20:13:49 -0500

    progreso en el front de la app

--- Front-End/scripts/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(3000, () => {
-    console.log('Server on port 3000');
-});
